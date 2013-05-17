8803021c0c25c9b10145b5c94fd7052e22f2529b
Desislav Petrov <DesIvanovPetrov@gmail.com>
2013-05-17 12:30:14 +0300

    TEst Comit

--- Source/KingSurvival.Test/ChessPiece.Test.cs
+++ Source/KingSurvival.Test/ChessPiece.Test.cs
@@ -8,8 +8,43 @@ namespace KingSurvivalTest
     public class ChessPieceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestChestPieceConstructorX()
         {
+            char excpected = 'K';
+            Coordinates newCoordinates = new Coordinates(2,3);
+            int x = newCoordinates.X;
+            int y = newCoordinates.Y;
+            ChessPiece testPiece = new ChessPiece(excpected,newCoordinates);
+            Assert.AreEqual(x, testPiece.X);
+        }
+
+        [TestMethod]
+        public void TestChestPieceConstructorY()
+        {
+            char excpected = 'K';
+            Coordinates newCoordinates = new Coordinates(2, 3);
+            int x = newCoordinates.X;
+            int y = newCoordinates.Y;
+            ChessPiece testPiece = new ChessPiece(excpected, newCoordinates);
+            Assert.AreEqual(y, testPiece.Y);
+        }
+
+        [TestMethod]
+        public void TestChestPieceSecondaryConstructorX()
+        {
+            char excpected = 'K';            
+            ChessPiece testPiece = new ChessPiece(excpected, 2 ,3);
+            int x = 2;
+            Assert.AreEqual(x, testPiece.X);
+        }
+
+        [TestMethod]
+        public void TestChestPieceSecondaryConstructorY()
+        {
+            char excpected = 'K';
+            ChessPiece testPiece = new ChessPiece(excpected, 2, 3);
+            int y = 3;
+            Assert.AreEqual(y, testPiece.Y);
         }
     }
 }
